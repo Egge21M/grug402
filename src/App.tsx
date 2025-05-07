298b3839bbead3660bed013dfbff7903d79d009f
Egge <egge@getcurrent.io>
2025-05-07 12:02:43 +0200

    added cashu payment to app

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Modal from "./components/Modal";
 import { useLightning } from "./hooks/useLightning";
 import QRCode from "react-qr-code";
 import { useEffect, useState } from "react";
+import { useCashu } from "./hooks/useCashu";
 
 function App() {
   const request = useRequest();
   const [quote, getQuote, lightningToken] = useLightning(request);
+  const [creq, createCreq, cashuToken] = useCashu(request);
   const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
@@ -28,11 +30,26 @@ function App() {
         );
         setImageUrl(`data:image/png;base64,${base64}`);
       }
+      if (cashuToken) {
+        const res = await fetch("https://cashu402.fly.dev/image", {
+          headers: {
+            "X-Cashu": cashuToken,
+          },
+        });
+        const buffer = await res.arrayBuffer();
+        const base64 = btoa(
+          new Uint8Array(buffer).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            "",
+          ),
+        );
+        setImageUrl(`data:image/png;base64,${base64}`);
+      }
     }
-    if (lightningToken) {
+    if (lightningToken || cashuToken) {
       getImage();
     }
-  }, [lightningToken]);
+  }, [lightningToken, cashuToken]);
   return (
     <main className="flex flex-col gap-4 items-center">
       <h1>Hot Memes For Sale!</h1>
@@ -45,7 +62,10 @@ function App() {
             <p>Pay {request.amount} Sats to see the meme!</p>
           </div>
           <div className="flex gap-4">
-            <button className="flex gap-2 items-center bg-purple-800">
+            <button
+              className="flex gap-2 items-center bg-purple-800"
+              onClick={createCreq}
+            >
               <TbBrandPeanut />
               Pay with Nuts
             </button>
@@ -66,6 +86,15 @@ function App() {
           <div className="p-2 bg-white rounded">
             <QRCode value={quote.request} />
           </div>
+        </Modal>
+      ) : undefined}
+      {creq && !cashuToken ? (
+        <Modal>
+          <p>Please pay this Cashu Payment Request</p>
+          <div className="p-2 bg-white rounded">
+            <QRCode value={creq} />
+          </div>
+          <p className="max-w-full text-xs wrap-anywhere">{creq}</p>
         </Modal>
       ) : undefined}
     </main>
