f62c8980763eb6afb8e2c6ac0326d5546e1b81eb
Egge <egge@getcurrent.io>
2025-05-07 16:38:39 +0200

    added explaination

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,18 @@ import { useLightning } from "./hooks/useLightning";
 import QRCode from "react-qr-code";
 import { useEffect, useState } from "react";
 import { useCashu } from "./hooks/useCashu";
+import mermaid from "mermaid";
 
 function App() {
   const request = useRequest();
   const [quote, getQuote, lightningToken] = useLightning(request);
   const [creq, createCreq, cashuToken] = useCashu(request);
   const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    mermaid.initialize({ theme: "dark" });
+    mermaid.contentLoaded();
+  });
 
   useEffect(() => {
     async function getImage() {
@@ -51,7 +57,7 @@ function App() {
     }
   }, [lightningToken, cashuToken]);
   return (
-    <main className="flex flex-col gap-4 items-center">
+    <main className="flex flex-col gap-4 items-center max-w-5xl w-full">
       <h1>Hot Memes For Sale!</h1>
       <div className="h-96 w-96 bg-neutral-700 rounded flex items-center justify-center">
         {imageUrl ? <img src={imageUrl} /> : <FaLock className="text-5xl" />}
@@ -97,6 +103,30 @@ function App() {
           <p className="max-w-full text-xs wrap-anywhere">{creq}</p>
         </Modal>
       ) : undefined}
+      <hr className="w-full my-8" />
+      <div className="flex flex-col gap-1 text-justify">
+        <h2 className="text-2xl font-bold my-4  ">How does this work?</h2>
+        <p>
+          The little Proof Of Concept you see above was built using Cashu 402, a
+          protocol for internet payments.
+        </p>
+        <p>
+          Cashu 402 uses the long-forgotten HTTP status quote 402 (Payment
+          Required). With it servers can communicate that a certain resource is
+          protected and needs to be paid. Clients can then add a Cashu token to
+          their request (using the X-Cashu header) to pay for it.
+        </p>
+        <pre className="mermaid flex flex-col w-full items-center text-center mt-8">
+          {`
+sequenceDiagram
+    Client->>Server: Request resource
+    Server->>Client: Status: 402 + CREQ in header
+    Note over Client: Parse CREQ & creates token
+    Client->>Server: Request resource (with token in header)
+    Server->>Client: Status: 200 + resource
+`}
+        </pre>
+      </div>
     </main>
   );
 }
